d5e52b21d835a71ecd681aae8f474ae25fdd139b
Anthony Tran <anthony.tran@syntouch.nl>
2026-09-09 18:42:00 +0200

    Fix setPassed and use unique id for decision definition in transaction

--- src/main/java/nl/syntouch/dmn/studio/service/UnitTestService.java
+++ src/main/java/nl/syntouch/dmn/studio/service/UnitTestService.java
@@ -3,7 +3,6 @@ package nl.syntouch.dmn.studio.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.quarkus.security.identity.SecurityIdentity;
 import jakarta.enterprise.context.ApplicationScoped;
@@ -26,6 +25,7 @@ import nl.syntouch.dmn.studio.repository.DmnVersionRepository;
 import org.openapi.quarkus.operaton_rest_api_json.api.DecisionDefinitionApi;
 import org.openapi.quarkus.operaton_rest_api_json.model.EvaluateDecisionDto;
 import org.openapi.quarkus.operaton_rest_api_json.model.VariableValueDto;
+
 import java.io.IOException;
 import java.net.URI;
 import java.util.*;
@@ -48,15 +48,27 @@ public class UnitTestService {
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
     public UnittestResultDTO handleTestDeployment(DeployTestDTO deployTestDTO) throws IOException {
-        DeploymentWithDefinitionsDto deploymentWithDefinitionsDto = createTestDeployment(deployTestDTO);
-        UnittestResultDTO result = callTestDeployment(deploymentWithDefinitionsDto, deployTestDTO);
-        deleteTestDeployment(deployTestDTO, deploymentWithDefinitionsDto.getId()); // https://github.com/awaitility/awaitility
+        DMN dmn = dmnRepository.findByIdOptional(deployTestDTO.dmnId()).orElseThrow();
+        DMNVersion dmnVersion = dmnVersionRepository.findByIdOptional(new DMNVersionId(dmn.getId(), deployTestDTO.version())).orElseThrow();
+
+        DeploymentWithDefinitionsDto deployment = createTestDeployment(deployTestDTO, dmn, dmnVersion);
+        String decisionDefinitionId = deployment.getDeployedDecisionDefinitions()
+                .entrySet().stream()
+                .filter(e -> deployTestDTO.decisionName().equals(e.getValue().getKey()))
+                .map(Map.Entry::getKey)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("No deployed decision found with key: " + deployTestDTO.decisionName()));
+
+        Test unitTest = getTest(deployTestDTO, dmnVersion);
+        unitTest.setDecisionDefinitionId(decisionDefinitionId);
+        unittestRepository.persist(unitTest);
+
+        UnittestResultDTO result = callTestDeployment(decisionDefinitionId, deployTestDTO);
+        deleteTestDeployment(deployment.getId()); // https://github.com/awaitility/awaitility
         return result;
     }
 
-    public DeploymentWithDefinitionsDto createTestDeployment(DeployTestDTO deployTestDTO) throws IOException {
-        DMN dmn = dmnRepository.findByIdOptional(deployTestDTO.dmnId()).orElseThrow();
-        DMNVersion dmnVersion = dmnVersionRepository.findByIdOptional(new DMNVersionId(dmn.getId(), deployTestDTO.version())).orElseThrow();
+    private DeploymentWithDefinitionsDto createTestDeployment(DeployTestDTO deployTestDTO, DMN dmn, DMNVersion dmnVersion) throws IOException {
         Environment test_env = Environment.find("name", "test").firstResult();
 
         DeployDTO deploymentData = new DeployDTO(
@@ -74,11 +86,7 @@ public class UnitTestService {
 
         var form = DmnDeploymentService.getCreateDeploymentMultipartForm(deploymentData, dmnVersion.getFileBlob());
         DeploymentApi unitTestClient = RestClientBuilder.newBuilder().baseUri(unitTestUrl + "/engine-rest").build(DeploymentApi.class);
-        DeploymentWithDefinitionsDto deploymentWithDefinitionsDto = unitTestClient.createDeployment(form);
-        Test unitTest = getTest(deployTestDTO, dmnVersion);
-        unittestRepository.persist(unitTest);
-
-        return deploymentWithDefinitionsDto;
+        return unitTestClient.createDeployment(form);
     }
 
     private static Test getTest(DeployTestDTO deployTestDTO, DMNVersion dmnVersion) {
@@ -114,15 +122,7 @@ public class UnitTestService {
         return unitTest;
     }
 
-    public UnittestResultDTO callTestDeployment(DeploymentWithDefinitionsDto deploymentWithDefinitionsDto, DeployTestDTO deployTestDTO) {
-        Optional<String> decisionDefinitionIdOpt = deploymentWithDefinitionsDto
-                .getDeployedDecisionDefinitions()
-                .keySet()
-                .stream()
-                .filter(key -> key.contains(deployTestDTO.decisionName()))
-                .findFirst();
-        String decisionDefinitionId = decisionDefinitionIdOpt.orElse(null);
-
+    public UnittestResultDTO callTestDeployment(String decisionDefinitionId, DeployTestDTO deployTestDTO) {
         try {
             DecisionDefinitionApi unitTestClient = RestClientBuilder.newBuilder()
                     .baseUri(URI.create(unitTestUrl + "/engine-rest"))
@@ -133,18 +133,15 @@ public class UnitTestService {
 
             String responseBody = objectMapper.writeValueAsString(result);
             boolean passed = compareDMNResponse(buildOutputJSON(deployTestDTO.outputData()), responseBody);
-            Test unitTest = unittestRepository.find("decisionName = ?1 AND dmnVersion.dmn.id = ?2 AND dmnVersion.version = ?3",
-                    deployTestDTO.decisionName(),
-                    deployTestDTO.dmnId(),
-                    deployTestDTO.version()).firstResult();
+
+            Test unitTest = unittestRepository.find("decisionDefinitionId", decisionDefinitionId).firstResult();
             unitTest.setPassed(passed);
-            unittestRepository.persist(unitTest);
 
             return new UnittestResultDTO(passed, buildOutputJSON(deployTestDTO.outputData()), responseBody);
         } catch (Exception e) {
             throw new RuntimeException(e);
-         }
-     }
+        }
+    }
 
     private EvaluateDecisionDto buildEvaluateDecisionDto(List<DeployTestDTO.ParamDTO> inputData) {
         Map<String, VariableValueDto> variables = new LinkedHashMap<>();
@@ -163,7 +160,7 @@ public class UnitTestService {
         ObjectNode combinedObject = objectMapper.createObjectNode();
         for (DeployTestDTO.ParamDTO param : outputData) {
             ObjectNode valueObject = objectMapper.createObjectNode();
-            String capitalized = param.typeRef().isEmpty() ? param.typeRef() : param.typeRef().substring(0,1).toUpperCase() + param.typeRef().substring(1);
+            String capitalized = param.typeRef().isEmpty() ? param.typeRef() : param.typeRef().substring(0, 1).toUpperCase() + param.typeRef().substring(1);
             valueObject.put("value", param.value());
             valueObject.put("type", capitalized);
             valueObject.set("valueInfo", objectMapper.createObjectNode()); // empty object
@@ -178,7 +175,7 @@ public class UnitTestService {
         return expectedArray.equals(actualArray);
     }
 
-    public void deleteTestDeployment(DeployTestDTO deployTestDTO, String deploymentId) {
+    public void deleteTestDeployment(String deploymentId) {
         try {
             DeploymentApi unitTestClient = RestClientBuilder.newBuilder().baseUri(unitTestUrl + "/engine-rest").build(DeploymentApi.class);
             unitTestClient.deleteDeployment(deploymentId, true, true, true);
