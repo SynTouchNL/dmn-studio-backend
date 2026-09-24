410bb56f0b2e659c462a48af98ae0ee16f0aafd9
Deniz Erdem <denizerdem18@gmail.com>
2026-09-24 13:22:51 +0200

    renamed dto fields

--- src/main/java/nl/syntouch/dmn/studio/model/dto/DomainResponseDTO.java
+++ src/main/java/nl/syntouch/dmn/studio/model/dto/DomainResponseDTO.java
@@ -3,4 +3,4 @@ package nl.syntouch.dmn.studio.model.dto;
 import java.time.Instant;
 
 public record DomainResponseDTO(Long id, String name, String ownerId, String owner, boolean active,
-                                String createdBy, String editedBy, Instant createdAt, Instant editedAt) {}
+                                String createdBy, String modifiedBy, Instant createdDate, Instant modifiedDate) {}
